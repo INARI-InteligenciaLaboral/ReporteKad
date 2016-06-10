3551b4e861f543503e58f291f16b1e8103ac03e3
Santiago Iván <salmaraz@inteligencialaboral.com>
2016-06-10 13:41:18 -0500

    Solicitar y guardar ruta de archivo bd

---
+++ ReporteKad/Clases/IteractionBD.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SQLite;
+
+namespace ReporteKad.Clases
+{
+    public class IteractionBD
+    {
+        public static string ConectionSQL(string m_Cadena)
+        {
+            string m_Conexion = "Data Source=" + m_Cadena + ";Version=3;";
+            SQLiteConnection objcon = new SQLiteConnection(m_Conexion);
+            SQLiteCommand objcomand;
+            SQLiteDataReader objreader;
+            try
+            {
+                objcon.Open();
+                objcomand = objcon.CreateCommand();
+                objcomand.CommandText = "SELECT hr_employee.id || ' ' || emp_firstname || ' ' || emp_lastname as Employee, att_punches.punch_time FROM att_punches INNER JOIN hr_employee ON att_punches.emp_id = hr_employee.id ORDER BY hr_employee.id, punch_time desc";
+                objreader = objcomand.ExecuteReader();
+                while (objreader.Read())
+                {
+                    
+                }
+            }
+            catch { }
+            return "";
+        }
+    }
+}
---
+++ ReporteKad/Clases/RutaBD.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReporteKad.Clases
+{
+    public class RutaBD
+    {
+        public static string BDConection()
+        {
+            string m_UbicacionArchivo = "";
+            if (!File.Exists(Application.StartupPath + "\\RutaBD.txt"))
+            {
+                m_UbicacionArchivo = solicitarRutaBD();
+                crearArchivo(m_UbicacionArchivo);
+            }
+            else
+            {
+                string line;
+                StreamReader file = new StreamReader(Application.StartupPath + "\\RutaBD.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    m_UbicacionArchivo = line;
+                }
+                file.Close();
+                if (!File.Exists(m_UbicacionArchivo))
+                {
+                    File.Delete(Application.StartupPath + "\\RutaBD.txt");
+                    m_UbicacionArchivo = solicitarRutaBD();
+                    crearArchivo(m_UbicacionArchivo);
+                }
+            }
+            return m_UbicacionArchivo;
+        }
+
+        private static string solicitarRutaBD()
+        {
+            string m_Ubicacion = "";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Archivos db y SQLite (*.db;*.SQLite)|*.db;*.SQLite";
+            dialog.Title = "Seleccione el archivo db";
+            dialog.FileName = string.Empty;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+               m_Ubicacion = dialog.FileName;
+            }
+            return m_Ubicacion;
+        }
+        private static void crearArchivo(string m_UbicacionArchivo)
+        {
+            string m_archivo = Application.StartupPath + "\\RutaBD.txt";
+            using (var fileStream = File.Create(m_archivo))
+            {
+                var texto = new UTF8Encoding(true).GetBytes(m_UbicacionArchivo);
+                fileStream.Write(texto, 0, texto.Length);
+                fileStream.Flush();
+            }
+        }
+    }
+}
