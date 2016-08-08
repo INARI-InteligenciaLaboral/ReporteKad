b505dce5464241a38ae2bc954f4a4671d93a1666
Santiago Iván Almaraz Segovia <salmaraz@inteligencialaboral.com>
2016-08-08 13:41:10 -0500

    Modificacion del diseño del reporte

--- ReporteKad/Clases/GenerarInsidencias.cs
+++ ReporteKad/Clases/GenerarInsidencias.cs
@@ -23,6 +23,38 @@ namespace ReporteKad.Clases
 
 
             string m_Reporte = string.Empty;
+            xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel)).Merge(false);
+            chartRange = xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel));
+            chartRange.FormulaR1C1 = "Registro de Asistencia";
+            chartRange.HorizontalAlignment = 3;
+            chartRange.VerticalAlignment = 3;
+            chartRange.Font.Size = 12;
+            chartRange.Font.Bold = true;
+            m_filaexcel++;
+            xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel)).Merge(false);
+            chartRange = xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel));
+            chartRange.FormulaR1C1 = "Reporte del " + m_FecInicio.ToString("dd/MM/yyyy") + " al " + m_FecFin.ToString("dd/MM/yyyy");
+            chartRange.HorizontalAlignment = 3;
+            chartRange.VerticalAlignment = 3;
+            chartRange.Font.Size = 12;
+            chartRange.Font.Bold = true;
+            m_filaexcel++;
+            xlWorkSheet.Cells[m_filaexcel, 1] = "Nombre";
+            DateTime m_fec = m_FecInicio;
+            for (int x = 2; m_FecFin >= m_fec; x++)
+            {
+                if (((int)m_fec.DayOfWeek) != 0)
+                {
+                    xlWorkSheet.Cells[m_filaexcel, x + 1] = m_fec.ToString("dddd\n dd");
+                    m_fec = m_fec.AddDays(1);
+                }
+                else
+                {
+                    m_fec = m_fec.AddDays(1);
+                    x--;
+                }
+            }
+            m_filaexcel++;
             foreach (var m_Row in m_empleados)
             {
                 var m_ObjEmp = (Modelos.Empleados)m_Row;
@@ -34,36 +66,14 @@ namespace ReporteKad.Clases
                     m_Horario = generarHorMin(m_cadena);
                 m_permisos = IteractionBD.ObtenerPermisos(m_ObjEmp.Id, m_FecInicio, m_FecFin, m_cadena);
                 m_Registros = IteractionBD.ObtenerRegistros(m_ObjEmp.Id, m_FecInicio, m_FecFin, m_cadena);
-                xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel)).Merge(false);
-                chartRange = xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel));
-                chartRange.FormulaR1C1 = "Registro de Asistencia";
-                chartRange.HorizontalAlignment = 3;
-                chartRange.VerticalAlignment = 3;
-                chartRange.Font.Size = 12;
-                chartRange.Font.Bold = true;
-                m_filaexcel++;
-
-                xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel)).Merge(false);
-                chartRange = xlWorkSheet.get_Range("a" + m_filaexcel, "i" + (m_filaexcel));
-                chartRange.FormulaR1C1 = m_ObjEmp.NombreCompleto;
-                chartRange.HorizontalAlignment = 3;
-                chartRange.VerticalAlignment = 3;
-                chartRange.Font.Size = 10;
-                m_filaexcel++;
-
-                xlWorkSheet.Cells[m_filaexcel, 1] = "Fecha";
-                xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                xlWorkSheet.Cells[m_filaexcel, 3] = "Incidencia";
-                xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                xlWorkSheet.Cells[m_filaexcel, 5] = "Hora de Entrada";
-                xlWorkSheet.Cells[m_filaexcel, 6] = "";
-                xlWorkSheet.Cells[m_filaexcel, 7] = "Hora de Salida";
-                xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                xlWorkSheet.Cells[m_filaexcel, 9] = "Comentarios";
-                m_filaexcel++;
+                xlWorkSheet.Cells[m_filaexcel, 1] = m_ObjEmp.NombreCompleto;
+                xlWorkSheet.Cells[m_filaexcel, 2] = "Hor. Ent.";
+                xlWorkSheet.Cells[m_filaexcel + 1, 2] = "Hor. Sal.";
+                string m_Insidencias = "";
+                int x = 2;
                 while (m_FecFin >= m_FechaResp)
                 {
-                    DataRow[] r_DiaLaborable = m_Horario.Select("dia_semana = " + ((int) m_FechaResp.DayOfWeek).ToString());
+                    DataRow[] r_DiaLaborable = m_Horario.Select("dia_semana = " + ((int)m_FechaResp.DayOfWeek).ToString());
                     DateTime? m_HoraIni = null;
                     DateTime? m_HoraFin = null;
                     if (r_DiaLaborable.Length > 0)
@@ -74,16 +84,9 @@ namespace ReporteKad.Clases
                         {
                             foreach (DataRow rowresult in r_Permisos)
                             {
-                                xlWorkSheet.Cells[m_filaexcel, 1] = m_FechaResp.ToString("dd/MM/yyyy.");
-                                xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 3] = "Permisos";
-                                xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 5] = (DateTime.Parse(rowresult[1].ToString())).ToString("HH:mm:00");
-                                xlWorkSheet.Cells[m_filaexcel, 6] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 7] = (DateTime.Parse(rowresult[2].ToString())).ToString("HH:mm:00");
-                                xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 9] = rowresult[4].ToString();
-                                m_filaexcel++;
+                                if (!m_Insidencias.Equals(""))
+                                    m_Insidencias += "\n";
+                                m_Insidencias  += rowresult[4].ToString();
                             }
                         }
                         if (r_Registros.Length > 0)
@@ -98,56 +101,32 @@ namespace ReporteKad.Clases
                             }
                             if (((int)m_FechaResp.DayOfWeek) == 6)
                             {
-                                xlWorkSheet.Cells[m_filaexcel, 1] = m_FechaResp.ToString("dd /MM/yyyy.");
-                                xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 3] = "Sabado";
-                                xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 5] = m_HoraIni.Value.ToString("HH:mm:ss");
-                                xlWorkSheet.Cells[m_filaexcel, 6] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 7] = m_HoraFin.Value.ToString("HH:mm:ss");
-                                xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 9] = "Dia no laboral trabajado";
-                                m_filaexcel++;
+                                xlWorkSheet.Cells[m_filaexcel, x + 1] = m_HoraIni.Value.ToString("HH:mm");
+                                xlWorkSheet.Cells[m_filaexcel + 1, x + 1] = m_HoraFin.Value.ToString("HH:mm");
                             }
                             else
                             {
-                                xlWorkSheet.Cells[m_filaexcel, 1] = m_FechaResp.ToString("dd/MM/yyyy.");
-                                xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 3] = "Asistencia";
-                                xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 5] = m_HoraIni.Value.ToString("HH:mm:ss");
-                                xlWorkSheet.Cells[m_filaexcel, 6] = "";
+                                xlWorkSheet.Cells[m_filaexcel, x + 1] = m_HoraIni.Value.ToString("HH:mm");
                                 if (m_HoraIni == m_HoraFin)
                                 {
-                                    xlWorkSheet.Cells[m_filaexcel, 7] = "";
-                                    xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                                    xlWorkSheet.Cells[m_filaexcel, 9] = "No existe registro de salida";
+                                    xlWorkSheet.Cells[m_filaexcel + 1, x + 1] = "";
                                 }
                                 else
                                 {
-                                    xlWorkSheet.Cells[m_filaexcel, 7] = m_HoraFin.Value.ToString("HH:mm:ss");
-                                    xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                                    xlWorkSheet.Cells[m_filaexcel, 9] = "";
+                                    xlWorkSheet.Cells[m_filaexcel + 1, x + 1] = m_HoraFin.Value.ToString("HH:mm");
                                 }
-                                m_filaexcel++;
                             }
                         }
                         else
                         {
                             if (r_Permisos.Length == 0)
                             {
-                                xlWorkSheet.Cells[m_filaexcel, 1] = m_FechaResp.ToString("dd/MM/yyyy.");
-                                xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 3] = "Falta";
-                                xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 5] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 6] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 7] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                                xlWorkSheet.Cells[m_filaexcel, 9] = "No existen registros o permisos asignados";
-                                m_filaexcel++;
+                                xlWorkSheet.Cells[m_filaexcel, x + 1] = "Falta";
+                                xlWorkSheet.Cells[m_filaexcel + 1, x + 1] = "";
                             }
                         }
+                        if (((int)m_FechaResp.DayOfWeek) != 0)
+                            x++;
                     }
                     else
                     {
@@ -162,25 +141,20 @@ namespace ReporteKad.Clases
                                 }
                                 m_HoraIni = DateTime.Parse(rowresult[0].ToString());
                             }
-                            xlWorkSheet.Cells[m_filaexcel, 1] = m_FechaResp.ToString("dd /MM/yyyy.");
-                            xlWorkSheet.Cells[m_filaexcel, 2] = "";
-                            xlWorkSheet.Cells[m_filaexcel, 3] = "Sabado";
-                            xlWorkSheet.Cells[m_filaexcel, 4] = "";
-                            xlWorkSheet.Cells[m_filaexcel, 5] = m_HoraIni.Value.ToString("HH:mm:ss");
-                            xlWorkSheet.Cells[m_filaexcel, 6] = "";
-                            xlWorkSheet.Cells[m_filaexcel, 7] = m_HoraFin.Value.ToString("HH:mm:ss");
-                            xlWorkSheet.Cells[m_filaexcel, 8] = "";
-                            xlWorkSheet.Cells[m_filaexcel, 9] = "Dia no laboral trabajado";
-                            m_filaexcel++;
+                            xlWorkSheet.Cells[m_filaexcel, x + 1] = m_HoraIni.Value.ToString("HH:mm");
+                            xlWorkSheet.Cells[m_filaexcel + 1, x + 1] = m_HoraFin.Value.ToString("HH:mm");
                         }
+                        if (((int)m_FechaResp.DayOfWeek) != 0)
+                            x++;
                     }
                     m_FechaResp = m_FechaResp.AddDays(1);
                 }
+                xlWorkSheet.Cells[m_filaexcel, x + 1] = m_Insidencias;
                 m_filaexcel += 2;
                 m_FechaResp = m_FecInicio;
 
             }
-            chartRange = xlWorkSheet.get_Range("a1", "i" + (m_filaexcel));
+            chartRange = xlWorkSheet.get_Range("a1", "z" + (m_filaexcel));
             chartRange.Columns.AutoFit();
 
             xlWorkBook.SaveAs(m_ruta_archivo, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
